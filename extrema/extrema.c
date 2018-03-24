d893ad4a97f09c6dc4df7be12d2a81e0e575775f
Logan Goldberg <goldberg@grinnell.edu>
2018-03-24 12:35:09 -0500

    fixed golden_section_search

--- extrema/extrema.c
+++ extrema/extrema.c
@@ -6,12 +6,17 @@ double func1(double x) {
   return (x - 2) * (x - 2);
 }
 
+double func2(double x) {
+  return 4 - x * x;
+}
+
 int main(void) {
   
   double lower_bound = 0;
   double upper_bound = 4;
   
   printf("min: %lf\n", find_univariate_min(lower_bound, upper_bound, func1));
+  printf("max: %lf\n", find_univariate_max(-1, 3, func2));
 
   return 0;
   
--- extrema/golden_section_search.c
+++ extrema/golden_section_search.c
@@ -1,6 +1,8 @@
 #include "golden_section_search.h"
+#include <stdio.h>
 
-#define GOLDEN_RATIO (1 + sqrt(5)) / 2;
+#define GOLDEN_RATIO (1 + sqrt(5)) / 2
+#define REVERSE_GR GOLDEN_RATIO - 1
 
 static double TOL = 1E-6;
 static uint32_t MAX_ITER = 1000;
@@ -18,30 +20,44 @@ static double midpoint(double a, double b) {
 }
 
 static bool decide_min(double a, double b) {
-  return a <= b;
+  return a < b;
 }
 
 static bool decide_max(double a, double b) {
-  return a >= b;
+  return a > b;
 }
 
-static double golden_section_search(double lower_bound, double upper_bound, double (*extrema_func)(double), bool (*comparator)(double, double)) {
+/**
+ * 
+ *
+ *
+ */
+static double golden_section_search(double domain_lower_bound, double domain_upper_bound, double (*extrema_func)(double), bool (*comparator)(double, double)) {
 
-  double inward_step = (upper_bound - lower_bound) / GOLDEN_RATIO;
-  double candidate_lower_bound = upper_bound - inward_step;
-  double candidate_upper_bound = lower_bound + inward_step;
+  // Set an iteration counter to eventually terminate from the loop
+  uint32_t iter = 0;
+
+  double max_dist = domain_upper_bound - domain_lower_bound;
+  double lower_guestimate = domain_lower_bound + (1 - (REVERSE_GR)) * max_dist;
+  double upper_guestimate = domain_lower_bound + (REVERSE_GR) * max_dist;
   
-  while (fabs(candidate_upper_bound - candidate_lower_bound) >= TOL) {
-    if (comparator(extrema_func(candidate_lower_bound), extrema_func(candidate_upper_bound))) {
-      upper_bound = candidate_upper_bound;
+  while (fabs(upper_guestimate - lower_guestimate) >= TOL && iter < MAX_ITER) {
+    if (comparator(extrema_func(lower_guestimate), extrema_func(upper_guestimate))) {
+      // Supposing the extrema is on the 'left' side of the interval
+      domain_upper_bound = upper_guestimate;
+      upper_guestimate = lower_guestimate;
+      max_dist = domain_upper_bound - domain_lower_bound;
+      lower_guestimate = domain_lower_bound + (1 - (REVERSE_GR)) * max_dist;
     } else {
-      lower_bound = candidate_lower_bound;
+      // Supposing the extrema is on the 'right' side of the interval
+      domain_lower_bound = lower_guestimate;
+      lower_guestimate = upper_guestimate;
+      max_dist = domain_upper_bound - domain_lower_bound;
+      upper_guestimate = domain_lower_bound + (REVERSE_GR) * max_dist;
     }
-    inward_step = (upper_bound - lower_bound) / GOLDEN_RATIO;
-    candidate_lower_bound = upper_bound - inward_step;
-    candidate_upper_bound = lower_bound + inward_step;
+    iter++;
   }
-  return midpoint(upper_bound, lower_bound);
+  return midpoint(domain_upper_bound, domain_lower_bound);
 }
 
 double find_univariate_min(double lower_bound, double upper_bound, double (*extrema_func)(double)) {
